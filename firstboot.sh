d40d94296406770b378e30c9c40f98208267d649
erkuerbs <erkuerbs@gmx.com>
2022-03-02 23:06:08 +0100

    commit

--- firstboot.sh
+++ firstboot.sh
@@ -3,11 +3,12 @@
 #systemd-machine-id-setup
 cp /root/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
 cp /root/interfaces /etc/network/interfaces
-export DEBIAN_FRONTEND=noninteractive
-apt install -q -y keyboard-configuration console-setup
+#
+#export DEBIAN_FRONTEND=noninteractive
+#apt install -q -y keyboard-configuration console-setup
 #apt install keyboard-configuration console-setup
-debconf-set-selections < /root/MyDebianAutomatedInstall/selections.conf
-dpkg-reconfigure keyboard-configuration -f noninteractive
+#debconf-set-selections < /root/MyDebianAutomatedInstall/selections.conf
+#dpkg-reconfigure keyboard-configuration -f noninteractive
 #if [ -f /etc/machine-id ]; then
 #systemctl --force reboot
 #fi
