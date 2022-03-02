19dde44cff36ac59de9023034c058c0f1517496d
erkuerbs <erkuerbs@gmx.com>
2022-03-02 17:44:24 +0100

    update

--- firstboot.sh
+++ firstboot.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#systemd-machine-id-setup
+systemd-machine-id-setup
 cp /root/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
 cp /root/interfaces /etc/network/interfaces
-#if [ -f /etc/machine-id ]; then
-#  systemctl --force reboot
-#fi
+if [ -f /etc/machine-id ]; then
+  systemctl --force reboot
+fi
