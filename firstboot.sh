eb1ffe7e6d504de8be357fa1bc7c6157f33d1685
erkuerbs <erkuerbs@gmx.com>
2022-03-02 15:06:00 +0100

    Next one

---
+++ firstboot.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+systemd-machine-id-setup
+cp /root/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
+cp /root/interfaces /etc/network/interfaces
+if [ -f /etc/machine-id ]; then
+  systemctl --force --force reboot
+endif
